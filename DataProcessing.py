4caa5d89718ef3ba6fa62736a41440c8669c7bcb
Dmitrii Korliakov <korliakov.dk@phystech.edu>
2023-05-08 00:55:04 +0300

    Basic functionality

---
+++ DataProcessing.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import scipy.stats as sps
+
+
+
+class DataProcessor:
+
+    def __init__(self, figs_path, figs_amount):
+        self.figs_path = figs_path
+        self.figs_amount = figs_amount
+
+
+    def create_figures(self):
+        pass
+
+    def make_grayscale(self):
+        pass
+
+    def resize(self):
+        pass
+
+
+    def augmentation(self):
+        pass
+
+class DataPreProcessor:
+
+    def make_grayscale(self):
+        pass
+
+    def resize(self):
+        pass
